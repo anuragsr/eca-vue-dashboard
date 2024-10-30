fb09a3357ecc1b127b9a8b59853146cb881004c3
anuragsr <anurag.131092@gmail.com>
2024-10-30 10:51:26 +0530

    Updated readme

---
+++ Readme.md
@@ -0,0 +1,21 @@
+## ECA Vue Dashboard
+A vuejs based dashboard with various components.
+
+[Demo](https://envisagecyberart.in/projects/dashboards/eca-vue-dashboard/)
+
+![Screenshot](Screenshot.png?raw=true)
+
+## Author
+[Anurag Srivastava](https://www.envisagecyberart.in)
+
+---
+
+<br/>
+Development:
+
+### `npm run dev` or `yarn dev`
+
+<br/>
+Production build:
+
+### `npm run build` or `yarn build`
---
+++ src/pages/Layout/ContentFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.envisagecyberart.in">Anurag</a>
+          </li>
+          <li>
+            <a href="https://www.envisagecyberart.in"> About Us </a>
+          </li>
+          <li>
+            <a href="https://www.envisagecyberart.in"> Blog </a>
+          </li>
+          <li>
+            <a href="https://www.envisagecyberart.in"> Licenses </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        &copy; {{ new Date().getFullYear() }}
+        <a href="https://www.creative-tim.com/?ref=mdf-vuejs" target="_blank"
+          >Anurag</a
+        >, made with <i class="fa fa-heart heart"></i> @ Envisage CyberArt
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
